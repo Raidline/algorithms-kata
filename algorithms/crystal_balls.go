7a2aba38a140247ae1e37256d1914094862b7008
Teixeira Fialho <teixeirafialho@MacBookPro.lan>
2025-12-31 21:15:32 +0000

    solves twoCristalBallsProblem

--- algorithms/crystal_balls.go
+++ algorithms/crystal_balls.go
@@ -1,5 +1,36 @@
 package algorithms
 
-func TwoCristalBalls(haystack []bool) int {
-	return 0
+import "math"
+
+func TwoCristalBalls(breaks []bool) int {
+
+	found := false
+	foundIdx := -1
+	size := len(breaks)
+	lastJump := 0
+	i := int(math.Floor(math.Sqrt(float64(size))))
+	for i < size {
+
+		if breaks[i] {
+			for lastJump < i {
+
+				if breaks[lastJump] {
+					found = true
+					foundIdx = lastJump
+					break
+				}
+
+				lastJump++
+			}
+
+			if found {
+				break
+			}
+		} else {
+			lastJump = i
+			i += int(math.Floor(math.Sqrt(float64(size))))
+		}
+	}
+
+	return foundIdx
 }
