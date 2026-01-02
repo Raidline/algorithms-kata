9e4157fbb4b342e027e9c608691ed94855c8cc91
Teixeira Fialho <teixeirafialho@MacBookPro.lan>
2026-01-02 21:03:03 +0000

    implements bubble sort

--- algorithms/bubble_sort.go
+++ algorithms/bubble_sort.go
@@ -2,4 +2,19 @@ package algorithms
 
 func BubbleSort(arr []int) {
 
+	size := len(arr)
+	limit := size
+
+	for limit > 0 {
+		for i := 0; i < limit-1; i++ {
+
+			if arr[i] > arr[i+1] {
+				tmp := arr[i+1]
+				arr[i+1] = arr[i]
+				arr[i] = tmp
+			}
+		}
+
+		limit--
+	}
 }
