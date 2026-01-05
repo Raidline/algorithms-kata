26bae0520349580cf2252a1015f42a66248b7b2a
Teixeira Fialho <pedrofilipefialho19@gmail.com>
2026-01-05 21:28:13 +0000

    implements bfs

--- algorithms/btbfs.go
+++ algorithms/btbfs.go
@@ -3,5 +3,34 @@ package algorithms
 import "algos/kata/algorithms/model"
 
 func Bfs(head *model.BinaryNode, needle int) bool {
+	//queue := NewQueue() // should use the queue i created but it does not support generics, sadge
+	//using array for that
+	queue := make([]*model.BinaryNode, 0)
+
+	queue = append(queue, head)
+
+	for len(queue) != 0 {
+
+		next := queue[len(queue)-1]
+		queue = remove(queue, len(queue)-1)
+
+		if next.Value == needle {
+			return true
+		}
+
+		if next.Left != nil {
+			queue = append(queue, next.Left)
+		}
+
+		if next.Right != nil {
+			queue = append(queue, next.Right)
+		}
+	}
+
 	return false
+}
+
+// not the more perfomant and prettiest but gets the job done
+func remove(slice []*model.BinaryNode, s int) []*model.BinaryNode {
+	return append(slice[:s], slice[s+1:]...)
 }
