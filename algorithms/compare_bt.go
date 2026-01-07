08fb601270aa90cdbfedd43d2f4e023603724f07
Teixeira Fialho <pedrofilipefialho19@gmail.com>
2026-01-07 21:01:44 +0000

    implements compareTree

--- algorithms/compare_bt.go
+++ algorithms/compare_bt.go
@@ -3,5 +3,18 @@ package algorithms
 import "algos/kata/algorithms/model"
 
 func CompareTrees(a *model.BinaryNode, b *model.BinaryNode) bool {
-	return false
+
+	if a == nil && b == nil {
+		return true
+	}
+
+	if a == nil || b == nil {
+		return false
+	}
+
+	if a.Value != b.Value {
+		return false
+	}
+
+	return CompareTrees(a.Left, b.Left) && CompareTrees(a.Right, b.Right)
 }
